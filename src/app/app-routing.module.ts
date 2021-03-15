1dee44bf3e5b48cae266303f6ba6515a7d5ab46a
Maximilian Krüger <Marcus.Aurelius@gallimathias.de>
2021-03-15 19:24:16 +0100

    add index
    * Add content to the app-component and add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BoardComponent } from './board/board.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/board' }, //Main page
+  // {path: '**', redirectTo: ''}, //404 File not Found
+  { path: 'board', component: BoardComponent }, //Board with no id
+  { path: 'board/:id', component: BoardComponent }, //Board with id
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,46 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { BoardCardComponent } from './board-card/board-card.component';
+import { CardListComponent } from './card-list/card-list.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { DrawerRailModule } from 'angular-material-rail-drawer';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    BoardCardComponent,
+    CardListComponent,
+    TopBarComponent,
   ],
   imports: [
+    //Angular
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    //Material
+    MatListModule,
+    MatCardModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+
+    //Custom
+    DrawerRailModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
